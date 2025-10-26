f5e8ac406776cdda3efedbf21a1e030e71500514
uzairpatel2001 <uzairpatel2001@gmail.com>
2025-10-26 16:38:03 +0530

    setcet token test

--- read_data_api.py
+++ read_data_api.py
@@ -1,9 +1,13 @@
 
 import requests
 import pandas as pd
+import os
 
-response = requests.get("https://jsonplaceholder.typicode.com/users")
-data  = response.json()
-df = pd.DataFrame(data)
-df = df[["id","name"]]
-print(df)
+token = os.getenv("API_TOKEN")
+print(f"TOKEN : {token}")
+
+#response = requests.get("https://jsonplaceholder.typicode.com/users")
+#data  = response.json()
+#df = pd.DataFrame(data)
+#df = df[["id","name"]]
+#print(df)
